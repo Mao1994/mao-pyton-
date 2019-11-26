668a4351b08fe6007883833d67922c1287df9595
Mauricio Rosero <mdroseroqu@unal.edu.co>
2019-11-26 12:57:13 -0500

    comente algunas lienas

--- OLX.py
+++ OLX.py
@@ -24,20 +24,21 @@ lista = []
 AYA = controlador.find_element_by_xpath('/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[4]/a[1]/span[1]')
 AYA.click()
 time.sleep(2)
+controlador.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+resultados = controlador.find_elements_by_css_selector('li.EIR5N')
+fecha = controlador.find_elements_by_css_selector('span.zLvFQ')
+si = fecha[len(fecha)-1].text
 
-
-
-clicks = 0
-while clicks < 5 :
-    controlador.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    time.sleep(2)
-    controlador.find_element_by_xpath('/html/body/div/div/main/div/section/div/div/div[4]/div[2]/div/div[3]/button').click()
-    resultados = controlador.find_elements_by_css_selector('li.EIR5N')
-    fecha = controlador.find_elements_by_css_selector('span.zLvFQ')
-    fecha[len(fecha)-1].text
-    clicks += 1
-    print("Numero de clicks %d. \n " % clicks)
-print("Utilizaste %d clicks ." % clicks)
+#SI="HOY"
+#clicks = 0
+#while SI==si:
+  #  controlador.find_element_by_xpath('/html/body/div/div/main/div/section/div/div/div[4]/div[2]/div/div[3]/button').click()
+   # resultados = controlador.find_elements_by_css_selector('li.EIR5N')
+    #fecha = controlador.find_elements_by_css_selector('span.zLvFQ')
+   # fecha[len(fecha)-1].text
+    #clicks += 1
+    #print("Numero de clicks %d. \n " % clicks)
+#print("Utilizaste %d clicks ." % clicks)
 #controlador.quit()
 #resultados = controlador.find_elements_by_css_selector('li.EIR5N')
 #fecha = controlador.find_elements_by_css_selector('span.zLvFQ')
